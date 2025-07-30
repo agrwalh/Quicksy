47317da8f174a09886d3a1ec69862ddcc99b990f
HA <haagarwal67@gmail.com>
2025-07-30 08:43:13 +0530

    Remove debug code - app is now working

--- routes/payment.js
+++ routes/payment.js
@@ -8,10 +8,6 @@ const { orderModel } = require("../models/order");
 const { cartModel } = require("../models/cart");
 const { deliveryModel } = require("../models/delivery");
 
-// Debug: Check if environment variables are loaded
-console.log('RAZORPAY_KEY_ID:', process.env.RAZORPAY_KEY_ID ? 'SET' : 'NOT SET');
-console.log('RAZORPAY_KEY_SECRET:', process.env.RAZORPAY_KEY_SECRET ? 'SET' : 'NOT SET');
-
 const razorpay = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET,
